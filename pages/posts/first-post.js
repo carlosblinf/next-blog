2383ddca758be54c01cc152ad24ee8c5aecee2c6
Carlos Ernesto <carlosblinf@gmail.com>
2022-05-26 19:44:08 -0400

    create first post component and adding some styles

---
+++ pages/posts/first-post.js
@@ -0,0 +1,24 @@
+import Layout from '../../components/layout'
+import Head from 'next/head'
+import Script from 'next/script'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <h1>First Post</h1>
+      <h2>
+        This is the first Post
+      </h2>
+    </Layout>
+  );
+}
